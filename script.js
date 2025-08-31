89c3a45a1db7db1382de81d3f660ed6153466f98
zabi <zabikhan5567@gmail.com>
2025-08-31 21:05:15 +0430

    use arrow function

--- script.js
+++ script.js
@@ -341,14 +341,15 @@
 // }
 // vowelCount("afghanistan");
 
-function vowel(st){
-    let count = 0;
-    for(let afg of st){
-        if( afg === "a" || afg === "e" || afg === "i"|| afg === "o"|| afg === "u" || afg === "A" || afg === "E" || afg === "I" || afg === "O" || afg === "U"){
-            count++;
-        }
-    }
-    console.log(count);
-}
+//use arrow function 
 
-vowel("aaffggaarruuoo oo");
+const vowel = (str) => {
+   let count = 0;
+   for(let char of str){
+       if( char === "a" || char === "e" || char === "i" || char === "o" || char === "u" || char === "A" || char === "E" || char === "I" || char === "O" || char === "U"){
+           count++;
+       }
+   }
+   console.log(count);
+}
+vowel("afghanistan");
